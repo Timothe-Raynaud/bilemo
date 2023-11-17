43d57a0e9a2e41b1b4fbadb35ea48e65aac9b71c
Timothe Raynaud <raynaud.timothe@gmail.com>
2023-11-17 10:13:41 +0100

    :construction: Add Entities

---
+++ src/Entity/Product.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\MappedSuperclass]
+abstract class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    protected ?int $id = null;
+
+    #[ORM\Column]
+    protected ?float $price = null;
+
+    #[ORM\Column(length: 255)]
+    protected ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    protected ?string $slug = null;
+
+    #[ORM\Column(length: 1000, nullable: true)]
+    protected ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'product')]
+    private ?Brand $brand = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getBrand(): ?Brand
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(?Brand $brand): static
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+}
---
+++ src/Repository/SmartphoneRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Smartphone;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Smartphone>
+ *
+ * @method Smartphone|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Smartphone|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Smartphone[]    findAll()
+ * @method Smartphone[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SmartphoneRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Smartphone::class);
+    }
+
+}
